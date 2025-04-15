353dbc937129ad34bb36993346d28c1fc5bbc2b2
Melos <melos.simeneh@gmail.com>
2025-04-15 11:52:45 +0300

    backend: production adjustment

--- backend/index.js
+++ backend/index.js
@@ -6,6 +6,7 @@ const mongoDBConnection = require("./config/db");
 const authRoutes = require("./routes/auth.routes");
 const cookieParser = require("cookie-parser");
 const { timestamp } = require("./utils/date");
+const path = require("path");
 
 const app = express();
 
@@ -17,6 +18,15 @@ app.use(cookieParser());
 app.use(cors({ origin: "http://localhost:5173", credentials: true }));
 
 app.use("/api/auth", authRoutes);
+
+if (process.env.NODE_ENV === "production") {
+  const __dirname = path.resolve();
+  app.use(express.static(path.join(__dirname, "/frontend/dist")));
+
+  app.get(/.*/, (req, res) => {
+    res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
+  });
+}
 
 const PORT = process.env.PORT;
 app.listen(PORT, () => {
